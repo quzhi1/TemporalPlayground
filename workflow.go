c904a74c58a13a8ff42043f1b220c1fc78a5a16d
Zhi Qu <quzhi65222714@gmail.com>
2022-03-08 17:40:42 -0800

    Call function by string. Use selector and future.

--- workflow.go
+++ workflow.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"time"
 
 	"go.temporal.io/sdk/workflow"
@@ -11,11 +12,19 @@ func GreetingWorkflow(ctx workflow.Context, name string) (string, error) {
 		StartToCloseTimeout: time.Second * 5,
 	}
 	ctx = workflow.WithActivityOptions(ctx, options)
+
 	var result1, result2 string
 	err := workflow.ExecuteActivity(ctx, ComposeGreeting, name).Get(ctx, &result1)
 	if err != nil {
 		return result1, err
 	}
-	err = workflow.ExecuteActivity(ctx, ComposeGoodbye, name).Get(ctx, &result2)
+
+	future := workflow.ExecuteActivity(ctx, "ComposeGoodbye", name)
+	selector := workflow.NewSelector(ctx)
+	selector.AddFuture(future, func(f workflow.Future) {
+		fmt.Println("selector running")
+		f.Get(ctx, &result2)
+	})
+	selector.Select(ctx)
 	return result1 + "; " + result2, err
 }
